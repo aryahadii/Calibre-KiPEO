460eebeda29491cf5e60d518e976d93345487e8f
Arya Hadi <arya.hadi97@gmail.com>
2019-01-16 19:13:21 +0330

    feat: replace bad thin spaces

--- reshaper.py
+++ reshaper.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import re
 from PyQt5.Qt import QAction, QInputDialog, QMessageBox
 from cssutils.css import CSSRule
@@ -12,25 +14,30 @@ from calibre.ebooks.oeb.polish.container import OEB_DOCS, OEB_STYLES, serialize
 from libpyarabicshaping import pyarabicshaping
 from io import open
 
+
 def reshape_book(container, verbose=False):
     for name, media_type in container.mime_map.iteritems():
         if media_type in OEB_DOCS:
 
-            if(verbose):
+            if (verbose):
                 print('processing ' + name)
 
             # read file content
             oed_doc_file_name = container.name_to_abspath(name)
             oeb_doc_file = open(oed_doc_file_name, 'r', encoding='utf-8')
             oeb_doc_content = oeb_doc_file.read()
-            oeb_doc_file.close()   
+            oeb_doc_file.close()
 
             # reshape and write the file
             oeb_doc_file = container.open(name, 'w')
+            oeb_doc_content = replace_bad_chars(oeb_doc_content)
             oeb_doc_content = pyarabicshaping.arabic_shape(oeb_doc_content)
             oeb_doc_file.write(oeb_doc_content)
             oeb_doc_file.close()
-        
 
             # flag files as dirty
             container.dirty(name)
+
+
+def replace_bad_chars(content):
+    return content.replace(u'\u200f', u'‌')
